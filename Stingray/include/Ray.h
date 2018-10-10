95ece8e3404f952d800114abb1db83e4dcddbfef
Alasdair Hitchen <alasdairhitchen@gmail.com>
2018-10-10 15:23:01 +0100

    Ray Class

--- Stingray/include/Ray.h
+++ Stingray/include/Ray.h
@@ -1,1 +1,18 @@
 #pragma once
+
+// cstdlib
+
+// external libs
+#include "GLM/common.hpp"
+
+// program
+
+namespace Stingray
+{
+	struct Ray
+	{
+		glm::vec3 origin;
+		glm::vec3 direction;
+		inline glm::vec3 point_at(const float _t) { return origin + _t * direction; }
+	};
+}
